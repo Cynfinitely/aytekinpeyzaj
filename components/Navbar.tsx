0f4a8188a92b8546d626f9b8d31d109a62a74386
Celal Yasin Nari <celalyasinnari@gmail.com>
2024-02-29 18:22:49 +0200

    search bar removed for now.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,20 +1,7 @@
-import { useState } from "react";
 import Link from "next/link";
 import styles from "../styles/Navbar.module.scss";
 
 const Navbar = () => {
-  const [searchTerm, setSearchTerm] = useState("");
-
-  const handleSearchChange = (event) => {
-    setSearchTerm(event.target.value);
-  };
-
-  const handleSearchSubmit = (event) => {
-    event.preventDefault();
-    // Implement your search functionality here
-    console.log(`Searching for "${searchTerm}"`);
-  };
-
   return (
     <nav className={styles.navbar}>
       <Link href="/" className={styles.navbar__link}>
@@ -29,18 +16,6 @@ const Navbar = () => {
       <Link href="/contact" className={styles.navbar__link}>
         Bize Ulaşın
       </Link>
-      <form onSubmit={handleSearchSubmit} className={styles.navbar__search}>
-        <input
-          type="text"
-          value={searchTerm}
-          onChange={handleSearchChange}
-          className={styles.navbar__searchInput}
-          placeholder="Search..."
-        />
-        <button type="submit" className={styles.navbar__searchButton}>
-          Search
-        </button>
-      </form>
     </nav>
   );
 };
