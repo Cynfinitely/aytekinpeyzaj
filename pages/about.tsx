cab9decd34545ce5c0fe7bb0d4d6b1e4d81debba
Celal Yasin Nari <celalyasinnari@gmail.com>
2024-03-01 21:41:54 +0200

    about page added.

---
+++ pages/about.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from "../styles/About.module.scss";
+
+const about = () => {
+  return (
+    <div className={styles.container}>
+      <h1>Hakkımızda</h1>
+      <p>
+        Firmamız 2020 yılından bu yana üretim, işleme, pazarlama ve satış
+        faaliyetlerini sürdürmektedir. Katı çiftlik gübresi, süper toprak, süper
+        humus, çim kapak harcı, torf, süper torf, fidan dikim harcı, çam
+        kabuğu,nebati toprak, orkide toprağı, karışım toprağı, solucan gübresi,
+        güvercin gübresi,oluşan 14 çeşit ürünüyle nihai tüketiciye, peyzaj,
+        inşaat ve turizm sektörünün paydaşlarına hitap etmektedir. Geniş üretim
+        yelpazesine sahip olan firmamız, konusunda uzman, teknik donanıma sahip,
+        bilgili ve becerikli çalışanlarıyla; teknolojik gelişmeye, ilerlemeye
+        açık şirket yapısı ve duruşuyla yakaladığı dünya standartlarını
+        istikrarlı bir biçimde sürdürüyor.
+      </p>
+    </div>
+  );
+};
+
+export default about;
