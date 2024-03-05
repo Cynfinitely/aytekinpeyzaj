678718183fb2e760e026a1205ef6f02e01c115ab
Celal Yasin Nari <celalyasinnari@gmail.com>
2024-03-05 21:19:22 +0200

    footer updated

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,6 +4,10 @@ export default function Footer() {
   return (
     <footer className={styles.footer}>
       <p>© 2024 Aytekin Peyzaj</p>
+      <p>
+        Adres: Ilıca Mah. E-80 Bulvarı Ilıca Mezarlıklar Karşısı Aziziye/Erzurum
+      </p>
+      <p>e-mail: aytekingnd@gmail.com</p>
       <p>İletişim: +90 551 849 45 24 , +90 539 339 63 04 </p>
     </footer>
   );
