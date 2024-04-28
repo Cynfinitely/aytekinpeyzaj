714543bdaeb0e71202549ae6783823aae8fbe28c
Celal Yasin Nari <celalyasinnari@gmail.com>
2024-04-28 16:44:44 +0300

    meta tags update.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,11 @@ class MyDocument extends Document {
         <Head>
           <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
           <meta
+            name="keywords"
+            content="Toprak Satışı, Bahçe Toprağı, Tarım Toprağı"
+          ></meta>
+          <meta name="author" content="Celal Yasin Nari"></meta>
+          <meta
             name="description"
             content="Aytekin Peyzaj, 2020'den beri üretim, işleme, ve pazarlama alanlarında faaliyet göstermektedir. Katı çiftlik gübresi, süper toprak, fidan dikim harcı ve daha birçok ürünle peyzaj, inşaat ve turizm sektörlerine hizmet veriyoruz. Uzman ekibimiz ve teknolojik alt yapımızla, dünya standartlarını yakalayarak müşterilerimize en kaliteli ürünleri sunuyoruz."
           />
