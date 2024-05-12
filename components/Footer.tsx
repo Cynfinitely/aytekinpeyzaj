f2866667ea612822b77ea94fca5aa2c822d8735c
Celal Yasin Nari <celalyasinnari@gmail.com>
2024-05-12 21:27:42 +0300

    styling update.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,6 @@
-import styles from "../styles/Footer.module.scss";
-
 export default function Footer() {
   return (
-    <footer className={styles.footer}>
+    <footer className="text-white bg-[#013440] flex flex-col items-center justify-evenly p-4 text-center sm:flex-row sm:justify-between sm:text-left">
       <p>© 2024 Aytekin Peyzaj</p>
       <p>
         Adres: Ilıca Mah. E-80 Bulvarı Ilıca Mezarlıklar Karşısı Aziziye/Erzurum
