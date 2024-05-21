a0978e9e5f6081f7a03dedd58cb4e776dcca0ffa
Celal Yasin Nari <celalyasinnari@gmail.com>
2024-05-21 23:09:31 +0300

    styling changes.

--- components/ProductList.tsx
+++ components/ProductList.tsx
@@ -1,10 +1,9 @@
 import { IProductListProps } from "../interfaces";
 import Product from "./Product";
-import styles from "../styles/ProductList.module.scss";
 
 const ProductList = (props: IProductListProps) => {
   return (
-    <div className={styles.products}>
+    <div className="flex flex-col justify-center items-center gap-6 pt-8">
       <h1>Ürünler</h1>
       {props.products.map((product, index) => (
         <Product product={product} key={index} />
