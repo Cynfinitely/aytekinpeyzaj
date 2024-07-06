bc00b50a5e4c1ee657d1e5c485a4d2f12f4f58a2
Celal Yasin Nari <celalyasinnari@gmail.com>
2024-07-06 14:59:27 +0300

    .

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -8,14 +8,14 @@ const contact = () => {
       <div className="flex justify-evenly items-center w-1/2 gap-12">
         <Image src="/whatsapp.png" alt="phone" width={50} height={50} />
         <p className="font-bold text-4xl">
-          <a href="https://wa.me/+905393396304">+90 539 339 63 04</a>
+          <a href="https://wa.me/+905393396304"> +90 539 339 63 04</a>
         </p>
       </div>
 
       <div className="flex justify-evenly items-center w-1/2 gap-12">
         <Image src="/whatsapp.png" alt="phone" width={50} height={50} />
         <p className="font-bold text-4xl">
-          <a href="https://wa.me/+905518494524">+90 551 849 45 24 </a>
+          <a href="https://wa.me/+905518494524"> +90 551 849 45 24 </a>
         </p>
       </div>
     </div>
